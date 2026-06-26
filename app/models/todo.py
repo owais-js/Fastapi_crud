f70d9bd7a290a7acf2600ed3c5fda05ad2e92d6e
owais-js <owaisshabbir3131@gmail.com>
2026-06-26 23:31:31 +0500

    todo model created and modified

---
+++ app/models/todo.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.core.database import Base
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
